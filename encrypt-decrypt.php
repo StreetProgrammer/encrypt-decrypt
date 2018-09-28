d5a8828fe5314f296c39ee2a9f509ce85cdc756d
StreetProgrammer <tahamostfa588@gmail.com>
2018-09-28 08:42:15 +0200

    Create encrypt-decrypt.php

---
+++ encrypt-decrypt.php
@@ -0,0 +1,28 @@
+/**
+ * Encrypt and decrypt
+ * 
+ * @author Nazmul Ahsan <n.mukto@gmail.com>
+ * @link http://nazmulahsan.me/simple-two-way-function-encrypt-decrypt-string/
+ *
+ * @param string $string string to be encrypted/decrypted
+ * @param string $action what to do with this? e for encrypt, d for decrypt
+ */
+function my_simple_crypt( $string, $action = 'e' ) {
+    // you may change these values to your own
+    $secret_key = 'my_simple_secret_key';
+    $secret_iv = 'my_simple_secret_iv';
+
+    $output = false;
+    $encrypt_method = "AES-256-CBC";
+    $key = hash( 'sha256', $secret_key );
+    $iv = substr( hash( 'sha256', $secret_iv ), 0, 16 );
+
+    if( $action == 'e' ) {
+        $output = base64_encode( openssl_encrypt( $string, $encrypt_method, $key, 0, $iv ) );
+    }
+    else if( $action == 'd' ){
+        $output = openssl_decrypt( base64_decode( $string ), $encrypt_method, $key, 0, $iv );
+    }
+
+    return $output;
+}
